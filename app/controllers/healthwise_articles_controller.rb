cb6ddff67290f7e039dc885a5a7c7a15d18799be
Justin Wong <justinw@cloudflare.com>
2023-08-25 09:21:10 -0700

    230823/migrations - move HWA token fetch logic to HWA controller; update CONSTANTS

--- app/controllers/healthwise_articles_controller.rb
+++ app/controllers/healthwise_articles_controller.rb
@@ -199,6 +199,22 @@ class HealthwiseArticlesController < ApplicationController
 
   private
 
+  require "rest-client"
+
+    def fetch_hw_token
+      Rails.cache.fetch("/healthwise_api_token", expires_in: 1.day) do
+        url = ENV['HEALTHWISE_AUTH_URL'] + "/oauth2/token"
+        response = RestClient::Request.execute(
+          method: :post,
+          url: url,
+          user: ENV['HEALTHWISE_CLIENT_ID'],
+          password: ENV['HEALTHWISE_CLIENT_SECRET'],
+          payload: "grant_type=client_credentials&scope=*"
+        )
+        JSON.parse(response.body)["access_token"]
+      end
+    end
+
     def fetch_languages(type, hwid)
       token = fetch_hw_token
       url = ENV['HEALTHWISE_CONTENT_URL'] + "/#{type}s/#{hwid}"
