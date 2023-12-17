d135919e24a4baf3c2e0f2e1d0cd5d7a6d33fbed
Justin Wong <justinw@cloudflare.com>
2023-12-17 05:10:39 -0800

    231216 update tag search functionality for Additionals

--- app/models/additional.rb
+++ app/models/additional.rb
@@ -1,6 +1,7 @@
 class Additional < ApplicationRecord
   extend FriendlyId
   friendly_id :en_title, use: %i(slugged history finders)
+
   scope :filter_by_search, -> (search) { where("en_title ilike ?", "%#{search}%").or(
                                          where("en_source ilike ?", "%#{search}%")).or(
                                          where("en_content ilike ?", "%#{search}%")).or(
@@ -25,10 +26,7 @@ class Additional < ApplicationRecord
                                          where("ko_source ilike ?", "%#{search}%")).or(
                                          where("ko_content ilike ?", "%#{search}%")).or(
                                          where("ko_notes ilike ?", "%#{search}%")).or(
-                                        #  where("tags @> ?", *values, "%#{search}%")).or(
-                                        #  where("tags[?] ilike ?", *values, "%#{search}%")).or(
-                                        #  where("tags ilike any (array[?])", "%#{search}%")).or(
-                                        #  where(tags.map {|tag| "tag ilike ?"}, "%#{search}%")).or(
+                                         where("array_to_string(tags,'||') ILIKE :en_title", en_title: "%#{search}%")).or(
                                          where("category ilike ?", "%#{search}%"))
                                        }
 end
